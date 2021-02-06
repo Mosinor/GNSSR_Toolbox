faf3a10de93704ae96c95ed3132dfd762a985af1
Strand94 <Andreasstrand94@gmail.com>
2021-02-06 20:03:24 +0100

    adding basic user inputs for the track demonstration tool

--- apps/toolbox/forms.py
+++ apps/toolbox/forms.py
@@ -1,18 +1,21 @@
 from django import forms
-from bootstrap_datepicker_plus import DatePickerInput
+from bootstrap_datepicker_plus import DatePickerInput, TimePickerInput
 from leaflet.forms.widgets import LeafletWidget
 from django.contrib.gis import forms
 
 
-class UserInputForm(forms.Form):
+class TrackDemoTool(forms.Form):
     grid = forms.PolygonField(widget=LeafletWidget(), required=False)
     coordinate_a = forms.CharField()
     coordinate_b = forms.CharField()
-    start_time = forms.DateField(widget=DatePickerInput().start_of('event days'))
-    end_time = forms.DateField(widget=DatePickerInput().end_of('event days'))
+    date = forms.DateField(widget=DatePickerInput().start_of('event days'))
+    start_time = forms.TimeField(widget=TimePickerInput().start_of('event days'))
+    end_time = forms.TimeField(widget=TimePickerInput().start_of('event days'))
+    level = forms.ChoiceField(choices=[('1', 'L1'), ('2', 'L2'), ('3', 'L3')])
+    version = forms.ChoiceField(choices=[('2', 'v2.1'), ('3', 'v3.0')])
 
     def clean(self):
-        cleaned_data = super(UserInputForm, self).clean()
+        cleaned_data = super(TrackDemoTool, self).clean()
 
 
 class SimpleExampleForm(forms.Form):
--- apps/toolbox/views.py
+++ apps/toolbox/views.py
@@ -1,23 +1,23 @@
 from django.shortcuts import render
-from .forms import UserInputForm
+from .forms import *
 
 
 # Form website where user defines input.
 def user_input(request):
     if request.method == 'POST':
-        form = UserInputForm(request.POST)
+        form = TrackDemoTool(request.POST)
         if form.is_valid():
-            start = form.cleaned_data['start_time']
-            end = form.cleaned_data['end_time']
+            print("Form submitted with data:")
 
     else:
-        form = UserInputForm()
+        form = TrackDemoTool()
 
     return render(request, "toolbox/user_selection.html", {'form': form})
 
 
 def tool_selection(request):
     return render(request, "toolbox/tool_selection.html",)
+
 
 def example(request):
     return render(request, "toolbox/example.html",)
