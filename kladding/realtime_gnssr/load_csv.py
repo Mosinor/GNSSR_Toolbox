6e3caac5adc775e25a52f79545d38d741cb576ba
Vegard Haneberg <vegard.haneberg@live.no>
2022-05-24 17:22:02 +0200

    added show/remove lines in chart and support for missing values in time series

--- kladding/realtime_gnssr/load_csv.py
+++ kladding/realtime_gnssr/load_csv.py
@@ -2,17 +2,10 @@ import pandas as pd
 from random import random
 import math
 import datetime
+import numpy as np
 
 
 def get_cygnss_time_series(start_date, end_date, region) -> list:
-
-    # cygnss_df = pd.read_csv('cygnss_data.csv')
-    # Convert the date input attributes to day number
-    # cygnss_df = filter_cygnss_date(cygnss_df, start_day, end_day)
-    # Maybe sort the dataframe on day_of_year to make sure the coordinates are correct?
-    # Do some averaging for each day. Maybe .group_by('date').mean() or something
-    # Include the possibility of varying the step size and the interval?
-    # return list(cygnss_df['sr'])
 
     area = {'north': region[0], 'south': region[2], 'west': region[1], 'east': region[3]}
 
@@ -26,15 +19,93 @@ def get_cygnss_time_series(start_date, end_date, region) -> list:
             current_df = pd.read_csv(path)[['lat', 'long', 'sr']]
             current_df = filter_location(current_df, area)
 
-            cygnss_ts.append(current_df['sr'].mean())
+            if len(current_df) > 0:
+                cygnss_ts.append(current_df['sr'].mean())
 
         start_date += delta
+
+    cygnss_ts = fill_missing_values(cygnss_ts)
+
+    if cygnss_ts is not None:
+        print('Length of cygnss ts:', len(cygnss_ts))
 
     return cygnss_ts
 
 
 def get_smap_time_series(start_date, end_date, region) -> list:
-    return [i*math.cos(i) for i in range(get_day_interval(start_date, end_date) + 1)]
+
+    area = {'north': region[0], 'south': region[2], 'west': region[1], 'east': region[3]}
+
+    base_path = 'data/SMAP '
+    delta = datetime.timedelta(days=1)
+    smap_ts = []
+
+    while start_date <= end_date:
+        if start_date.year in [2019, 2020, 2021]:
+            if start_date.day < 10:
+                day_string = '0' + str(start_date.day)
+            else:
+                day_string = str(start_date.day)
+            if start_date.month < 10:
+                month_string = '0' + str(start_date.month)
+            else:
+                month_string = str(start_date.month)
+
+            path = base_path + str(start_date.year) + '/' + month_string + '-' + day_string + '.csv'
+
+            try:
+                current_df = pd.read_csv(path)[['lat', 'long', 'smap_sm']]
+            except FileNotFoundError:
+                current_df = pd.DataFrame.from_dict({'lat': [], 'long': [], 'smap_sm': []})
+
+            current_df = filter_location(current_df, area)
+
+            if len(current_df) > 0:
+                smap_ts.append(current_df['smap_sm'].mean())
+            else:
+                smap_ts.append(-999)
+
+        start_date += delta
+
+    smap_ts = fill_missing_values(smap_ts)
+
+    if smap_ts is not None:
+        print('Length of SMAP time series:', len(smap_ts))
+
+    return smap_ts
+
+
+def fill_missing_values(ts):
+    for i in range(len(ts)):
+        if ts[i] == -999:
+            closest_back_value = find_closest_existing_value(ts, i, False)
+            closest_forward_value = find_closest_existing_value(ts, i, True)
+
+            if closest_back_value is None and closest_forward_value is None:
+                return None
+            if closest_back_value is not None and closest_forward_value is not None:
+                ts[i] = np.mean([closest_back_value, closest_forward_value])
+
+            elif closest_back_value is None:
+                ts[i] = closest_forward_value
+            elif closest_forward_value is None:
+                ts[i] = closest_back_value
+    return ts
+
+
+def find_closest_existing_value(ts, index, forward):
+    if forward:
+        while index < len(ts):
+            if ts[index] > 0:
+                return ts[index]
+            index = index + 1
+        return None
+    else:
+        while index >= 0:
+            if ts[index] > 0:
+                return ts[index]
+            index = index - 1
+        return None
 
 
 def filter_date(df, start_day, end_day, date_column_name='day_of_year') -> pd.DataFrame:
